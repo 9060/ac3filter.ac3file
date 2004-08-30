97867f79264cac458530be411a863b02443452d7
valex <valex@localhost>
2004-08-30 22:15:48 +0000

    Daily

---
+++ resource.h
@@ -0,0 +1,18 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by ac3file.rc
+//
+#define IDD_FILE                        106
+#define IDC_EDIT2                       1001
+#define IDC_BTN_CLOSE                   1002
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
